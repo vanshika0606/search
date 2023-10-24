ad78a170ce3eea3ed2a6367e6fd278829e49c317
Vanshika <vanshika06065@gmail.com>
2023-10-24 13:59:35 +0530

    added loader in detail section

--- src/Components/cardDetail/CardDetail.tsx
+++ src/Components/cardDetail/CardDetail.tsx
@@ -1,33 +1,50 @@
-import  {forwardRef} from "react";
+import { forwardRef } from "react";
 import { useAppContext } from "../../context/App.context";
 import { RxCross1 } from "react-icons/rx";
 import "./CardDetail.style.css";
+import Loader from "../loader/Loader";
 
 const CardDetail = forwardRef(() => {
-  const { cardDetail, setIsCardClick} = useAppContext();
-  
-  const handleCross = ()=>{
+  const { cardDetail, setIsCardClick } = useAppContext();
+
+  const handleCross = () => {
     setIsCardClick(false);
-  }
+  };
 
   return (
-    <div  className={` card-detail }`} >
-      <span className="cross" onClick={handleCross} >
-        <RxCross1 size="15" />
-      </span>
-      <h2 className="card-detail-heading text-center">{cardDetail?.title}</h2>
+    <>
+      {cardDetail ? (
+        <div className={` card-detail }`}>
+          <span className="cross" onClick={handleCross}>
+            <RxCross1 size="15" />
+          </span>
+          <h2 className="card-detail-heading text-center">
+            {cardDetail?.title}
+          </h2>
 
-      <p className="points text-right">Points : {cardDetail?.points}</p>
+          <p className="points text-right">Points : {cardDetail?.points}</p>
 
-        <p>Comments</p>
-        <div className="comments">
-        {cardDetail?.comments?.length ?
-        cardDetail.comments.map((comment, index) => {
-          return <div key={index} className="comment-card" dangerouslySetInnerHTML={{__html: comment}}></div>;
-        }): <h2>No Comments</h2>}
+          <p>Comments</p>
+          <div className="comments">
+            {cardDetail?.comments?.length ? (
+              cardDetail.comments.map((comment, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="comment-card"
+                    dangerouslySetInnerHTML={{ __html: comment }}
+                  ></div>
+                );
+              })
+            ) : (
+              <h2>No Comments</h2>
+            )}
+          </div>
         </div>
-    
-    </div>
+      ) : (
+        <Loader/>
+      )}
+    </>
   );
 });
 
